3f4d740ed2cd8942605ada0521fa7a1376bb9b63
et6341 <72764888+et6341@users.noreply.github.com>
2021-01-03 13:26:03 -0800

    Update main.ts, main.py

--- main.ts
+++ main.ts
@@ -1,1 +1,9 @@
-
+while (true) {
+    console.log("light Level: " + input.lightLevel())
+    if (input.lightLevel() < 13) {
+        light.setAll(light.rgb(255, 0, 0))
+    } else if (input.lightLevel() < 6) {
+        light.setAll(light.rgb(0, 0, 0))
+    }
+    
+}
